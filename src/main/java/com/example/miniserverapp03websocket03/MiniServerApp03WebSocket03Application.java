ab674b1260c54ef1ee58333cd65dc894aad2b1c5
Kota <maedakoutaa@gmail.com>
2022-12-04 09:02:49 +0900

    initial commit

---
+++ src/main/java/com/example/miniserverapp03websocket03/MiniServerApp03WebSocket03Application.java
@@ -0,0 +1,13 @@
+package com.example.miniserverapp03websocket03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniServerApp03WebSocket03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniServerApp03WebSocket03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/miniserverapp03websocket03/MiniServerApp03WebSocket03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniserverapp03websocket03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniServerApp03WebSocket03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
